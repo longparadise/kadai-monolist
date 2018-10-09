b88e2c9fc9b0dc2ba97de847699e88ee55268c41
ryuichirou.sugimoto <longparadise0422@gmail.com>
2018-10-09 07:37:07 +0000

    init for heroku

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,24 @@
+class ItemsController < ApplicationController
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+  end
+  
+  def new
+    @items = []
+    
+    @keyword = params[:keyword]
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imegeFlag: 1,
+        hits: 20,
+      })
+      
+      results.each do |result|
+        item = Item.find_or_initialize_by(read(result))
+        @items << item
+      end
+    end
+  end
+end
